cea7f4ccd38221381c4fb98f27b35f92eec5ec74
theXsingularity <jdspaziano@gmail.com>
2022-04-03 13:19:18 -0400

    fixed eventListener issue w/ border by adding background to squares

--- script.js
+++ script.js
@@ -1,47 +1,30 @@
 let square = document.createElement('div');
 square.classList.add('square');
-square.textContent = "X";
-
+square.textContent = "";
 
 let grid = document.getElementById('grid');
+
 function createGrid(num) {
   document.getElementById("grid").style.gridTemplateColumns = `repeat(${num}, 1fr)`;
   for(i=0; i<num*num; i++){
-          grid.appendChild(square.cloneNode(true));    
+          grid.appendChild(square.cloneNode(true));
   }
-  square.style.color = 'blue'
-  let gridStyle = document.getElementById('grid').style;
-  gridStyle.backgroundColor = 'coral';
 }
 
 
-grid.addEventListener('click', changeColor )
-
-
-function  changeColor(event) {
-    console.log(event.target);
-    event.target.style.background = 'green';
-  }
-
-
-createGrid(16);
-
-
-
-
-
-
-
-
-
-
-/*
-let btn = document.getElementById('btn');
-let item = document.getElementById("button");
-item.addEventListener("mouseover", func, false);
-
-function func()
-{  
-   item.setAttribute("style", "background-color:blue;")
+function  hoverGreen(event) {
+  //console.log(event.target);
+  event.target.style.background = 'black';
 }
-*/
+
+function clickRed(event){
+event.target.style.background = 'red';
+}
+
+grid.addEventListener('mouseover', hoverGreen)
+grid.addEventListener('click', clickRed)
+
+
+
+
+createGrid(60);
