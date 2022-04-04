df9c0e44becee3bc328ff4fc809faacf6de93fec
theXsingularity <jdspaziano@gmail.com>
2022-04-04 17:44:20 -0400

    grid is now static. Moved event listeners under createGrid

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-function turnRed(event){
-    console.log('test');
-    event.target.style.background = 'red';
-}
 
-function createGrid(num) { //create function that takes a number
-  document.getElementById("grid").style.gridTemplateColumns = `repeat(${num}, 1fr)`; //set col to num
-  for(i=0; i<num*num; i++){ //creates (num X num) divs
-    let square = document.createElement('div'); //creates element div stored in value square
-    square.classList.add('square'); //add class square to div
-    grid.appendChild(square); //add div to parent div(grid)
+
+//creates squares for grid
+let square = document.createElement('div'); //creates element div stored in value square
+square.classList.add('square'); //add class square to div
+
+
+//creates node-list of elements w/ className 'square'
+let squares = document.getElementsByClassName('square');
+
+//function that adds event listeners to squares
+
+
+
+//function that creates grid of divs for num*num times
+//sets columns to num (so num X num grid)
+function createGrid(num) {
+  square.style.height = (600/num)+"px"; //sets square size
+  square.style.width = (600/num)+"px";  
+  document.getElementById("grid").style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  for(i=0; i<num*num; i++){
+    grid.appendChild(square.cloneNode(true));
   }
-  let squares = document.getElementsByClassName('square'); //creates node-list of all classId square
   for (let i = 0; i <squares.length; i++) {  //loops through node-list
-  squares[i].addEventListener('click', turnRed); //adds turnRed function
-  squares[i].addEventListener('mouseover', function(e) { // adds event listener
-    e.target.style.background = 'gray';//add anon function turn background gray
-  });
-  }
+    squares[i].addEventListener('click', turnRed); //adds turnRed function (see below)
+    squares[i].addEventListener('mouseover', function(e) { // adds event listener
+      e.target.style.background = 'gray';//add anon function turn background gray
+    });
+    }
 }
 
+createGrid(6); //create grid
 
-createGrid(60);
+
+
+///could both the append chilc loop and add event listener loop happen else where
+//they dont seem to make sense in the code right there
+//how do I get the loops to run ...
+
+function turnRed(event){
+  console.log('test');
+  event.target.style.background = 'red';
+}
