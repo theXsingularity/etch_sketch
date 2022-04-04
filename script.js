1b8594eae43e12d719fe29edeaef44f2f0cff500
theXsingularity <jdspaziano@gmail.com>
2022-04-03 20:13:43 -0400

    event listener added to each square specifically (rather than the grid) - grid functionality 100% -  need dynamic grid buttons next

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-let square = document.createElement('div');
-square.classList.add('square');
-square.textContent = "";
+function turnRed(event){
+    console.log('test');
+    event.target.style.background = 'red';
+}
 
-let grid = document.getElementById('grid');
-
-function createGrid(num) {
-  document.getElementById("grid").style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-  for(i=0; i<num*num; i++){
-          grid.appendChild(square.cloneNode(true));
+function createGrid(num) { //create function that takes a number
+  document.getElementById("grid").style.gridTemplateColumns = `repeat(${num}, 1fr)`; //set col to num
+  for(i=0; i<num*num; i++){ //creates (num X num) divs
+    let square = document.createElement('div'); //creates element div stored in value square
+    square.classList.add('square'); //add class square to div
+    grid.appendChild(square); //add div to parent div(grid)
+  }
+  let squares = document.getElementsByClassName('square'); //creates node-list of all classId square
+  for (let i = 0; i <squares.length; i++) {  //loops through node-list
+  squares[i].addEventListener('click', turnRed); //adds turnRed function
+  squares[i].addEventListener('mouseover', function(e) { // adds event listener
+    e.target.style.background = 'gray';//add anon function turn background gray
+  });
   }
 }
-
-
-function  hoverGreen(event) {
-  //console.log(event.target);
-  event.target.style.background = 'black';
-}
-
-function clickRed(event){
-event.target.style.background = 'red';
-}
-
-grid.addEventListener('mouseover', hoverGreen)
-grid.addEventListener('click', clickRed)
-
-
 
 
 createGrid(60);
